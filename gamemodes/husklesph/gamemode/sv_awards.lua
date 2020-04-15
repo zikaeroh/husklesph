39cb6b9ff8d2ad5ee4ae1203c022ffd2cfc34b03
Yolopanther12 <Yolopanther12@users.noreply.github.com>
2020-04-14 21:25:39 -0700

    Added sv_awards file for managing endround awards
    The sv_awards.lua file is new system for managing the end of round
    player awards. All of the award types in prophunters have been moved to
    this file. This file easily supports adding new rewards by simply
    following the format of the awards already in the file.
    This commit simply checks in this file, it does not modify the existing
    code to use this new system.

---
+++ gamemodes/husklesph/gamemode/sv_awards.lua
@@ -0,0 +1,133 @@
+-- last prop death award
+local function lastPropStandingCalcuation()
+	if GAMEMODE.LastRoundResult == 2 then
+		return GAMEMODE.LastPropDeath
+	end
+
+	return nil
+end
+
+
+-- get prop with least movement
+local function leastMovementCalcuation()
+	local minPly
+
+	for _, ply in ipairs(GAMEMODE:GetPlayingPlayers()) do
+		if ply:Team() != 3 then continue end
+
+		if !minPly || ply.PropMovement < minPly.PropMovement then
+			minPly = ply
+		end
+	end
+
+	return minPly
+end
+
+
+-- get prop with most taunts
+local function mostTauntsCalcuation()
+	local maxPly
+
+	for _, ply in ipairs(GAMEMODE:GetPlayingPlayers()) do
+		if ply:Team() != 3 then continue end
+
+		if !maxPly || ply.TauntAmount > maxPly.TauntAmount then
+			maxPly = ply
+		end
+	end
+
+	return (maxPly && maxPly.TauntAmount > 0) && maxPly || nil
+end
+
+
+-- first hunter kill award
+local function firstHunterKillCalcuation()
+	return GAMEMODE.FirstHunterKill
+end
+
+
+-- get hunter with most kills
+local function mostKillsCalcuation()
+	local maxPly
+
+	for _, ply in ipairs(GAMEMODE:GetPlayingPlayers()) do
+		if ply:Team() != 2 then continue end
+
+		if !killsPly || ply.HunterKills > maxPly.HunterKills then
+			maxPly = ply
+		end
+	end
+
+	return (maxPly && maxPly.HunterKills > 0) && maxPly || nil
+end
+
+
+-- get hunter with most prop damage
+local function propDamageCalcuation()
+	local maxPly
+
+	for _, ply in ipairs(GAMEMODE:GetPlayingPlayers()) do
+		if ply:Team() != 2 then continue end
+
+		if !maxPly || ply.PropDmgPenalty > maxPly.PropDmgPenalty then
+			maxPly = ply
+		end
+	end
+
+	return (maxPly && maxPly.PropDmgPenalty > 0) && maxPly || nil
+end
+
+
+-- get prop with most movement
+local function mostMovementCalcuation()
+	local maxPly
+
+	for _, ply in ipairs(GAMEMODE:GetPlayingPlayers()) do
+		if ply:Team() != 3 then continue end
+
+		if !maxPly || ply.PropMovement > maxPly.PropMovement then
+			maxPly = ply
+		end
+	end
+
+	return (maxPly && maxPly.PropMovement > 0) && maxPly || nil
+end
+
+
+PlayerAwardsTemplate = {
+	LastPropStanding = {
+		name = "Longest Survivor",
+		desc = "Prop who survived longest",
+		getWinner = lastPropStandingCalcuation
+	},
+	LeastMovement = {
+		name = "Least Movement",
+		desc = "Prop who moved the least",
+		getWinner = leastMovementCalcuation
+	},
+	MostTaunts = {
+		name = "Most Taunts",
+		desc = "Prop who taunted the most",
+		getWinner = mostTauntsCalcuation
+	},
+	FirstHunterKill = {
+		name = "First Blood",
+		desc = "Hunter who had the first kill",
+		getWinner = firstHunterKillCalcuation
+	},
+	MostKills = {
+		name = "Most Kills",
+		desc = "Hunter who had the most kills",
+		getWinner = mostKillsCalcuation
+	},
+	PropDamage = {
+		name = "Angriest Player",
+		desc = "Hunter who shot at props the most",
+		getWinner = propDamageCalcuation
+	},
+	MostMovement = {
+		name = "Most Movement",
+		desc = "Prop who moved the most",
+		getWinner = mostMovementCalcuation
+	}
+}
