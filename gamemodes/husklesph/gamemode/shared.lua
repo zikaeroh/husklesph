8157d72fe91ed49dfa549952ab6538b77ee118be
Asdjky <36432064+Asdjky@users.noreply.github.com>
2020-05-04 23:01:58 -0700

    COOL STUFF ALL AROUND

--- gamemodes/husklesph/gamemode/shared.lua
+++ gamemodes/husklesph/gamemode/shared.lua
@@ -1,43 +1,82 @@
+
 local PlayerMeta = FindMetaTable("Player")
+
 local tabFile = file.Read(GM.Folder .. "/husklesph.txt", "GAME") || ""
+
 local tab = util.KeyValuesToTable(tabFile)
 
+
+
 GM.Name 	= tab["title"] || "Prop Hunters - Huskles Edition"
+
 GM.Author 	= "Zikaeroh, MechanicalMind"
+
 -- Credits to waddlesworth for the logo and icon
+
 GM.Email 	= "N/A"
+
 GM.Website 	= "N/A"
+
 GM.Version  = tab["version"] || "unknown"
 
+
 ROUND_WAIT = 1
+
 ROUND_HIDE = 2
+
 ROUND_SEEK = 3
+
 ROUND_POST = 4
+
 ROUND_MAPVOTE = 5
 
+
+
 TEAM_SPEC = 1
+
+
 TEAM_HUNTER = 2
+
 TEAM_PROP = 3
+
+
 
 WIN_NONE = TEAM_SPEC
 WIN_HUNTER = TEAM_HUNTER
+
 WIN_PROP = TEAM_PROP
 
+
+
 function PlayerMeta:IsSpectator() return self:Team() == TEAM_SPEC end
+
+
 function PlayerMeta:IsHunter() return self:Team() == TEAM_HUNTER end
 function PlayerMeta:IsProp() return self:Team() == TEAM_PROP end
 
+
+
 GM.StartWaitTime = CreateConVar("ph_mapstartwait", 30, bit.bor(FCVAR_NOTIFY, FCVAR_REPLICATED), "Number of seconds to wait for players on map start before starting round")
 
+
+
 team.SetUp(TEAM_SPEC, "Spectators", Color(120, 120, 120), false) -- Setting Joinable to false allows us to use team.BestAutoJoinTeam and have it only include the Hunters/Props teams.
+
 team.SetUp(TEAM_HUNTER, "Hunters", Color(255, 150, 50))
+
 team.SetUp(TEAM_PROP, "Props", Color(50, 150, 255))
+
+
 
 function GM:PlayerSetNewHull(ply, s, hullz, duckz)
 	self:PlayerSetHull(ply, s, s, hullz, duckz)
+
 end
 
+
+
 function GM:PlayerSetHull(ply, hullx, hully, hullz, duckz)
+
 	hullx = hullx || 16
 	hully = hully || 16
 	hullz = hullz || 72
