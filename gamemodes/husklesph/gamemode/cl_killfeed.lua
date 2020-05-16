924b50902109feddaa927d9f4d9246d30aba1d2e
Yolopanther <Yolopanther12@users.noreply.github.com>
2020-05-16 15:22:21 -0500

    Kill feed visual improvements (#23)

--- gamemodes/husklesph/gamemode/cl_killfeed.lua
+++ gamemodes/husklesph/gamemode/cl_killfeed.lua
@@ -1,67 +1,114 @@
 local killFeedEvents = {}
 
-local KILL_FEED_MESSAGE_TIMEOUT = 10 -- How long a message stays in the kill feed before starting to fade away
-local FADE_UPDATE_INTERVAL = 0.1 -- How long to wait before increasing the fade-ness of an expiring kill feed entry
-local FADE_TIME = 1.5 -- How long it takes for a kill feed entry to fade away
+local KILL_FEED_MESSAGE_TIMEOUT = 10 -- How long a message stays in the kill feed before starting to fade away.
+local VISUALS_UPDATE_INTERVAL = 0.05 -- Tick speed of the visual update timer. Smaller number = smoother visuals.
+local FADE_TIME = 1.5 -- How long it takes for an expiring kill feed message to fade away.
 
-local NUM_FADE_TICKS = math.floor(FADE_TIME / FADE_UPDATE_INTERVAL)
+local NUM_FADE_TICKS = math.floor(FADE_TIME / VISUALS_UPDATE_INTERVAL)
 local FADE_AMOUNT = math.floor(255 / NUM_FADE_TICKS)
+
+local FONT = "RobotoHUD-15" -- Font used for kill feed messages.
 
 function GM:ClearKillFeed()
 	killFeedEvents = {}
 end
 
--- This timer modifies the alpha values of each part of a kill feed message to create a "fading out" effect
-timer.Create("ph_timer_kill_feed", FADE_UPDATE_INTERVAL, 0, function()
-	local event = killFeedEvents[1]
-	if event && event.entryTime + KILL_FEED_MESSAGE_TIMEOUT < CurTime() then
-		if event.attackerName then
-			event.attackerColor.a = event.attackerColor.a - FADE_AMOUNT
-		end
-		event.victimColor.a = event.victimColor.a - FADE_AMOUNT
-		event.messageColor.a = event.messageColor.a - FADE_AMOUNT
-		if event.messageColor.a < 0 then
-			table.remove(killFeedEvents, 1)
+-- This timer handles the visual updates for kill feed messages.
+-- New messages are given a "slide in" effect wherein they slide in from off-screen.
+-- Expiring messages are given a fade out effect.
+timer.Create("ph_timer_kill_feed_visuals", VISUALS_UPDATE_INTERVAL, 0, function()
+	for i, event in ipairs(killFeedEvents) do
+		local finalPos = ScrW() - 10 - event.eventWidth
+		-- Handle slide in effect.
+		if event.new then
+			-- The distance to slide in a single "frame" is going to be proportional to how much distance is
+			-- left to the final position. This will be scaled up by 50% to make things go faster.
+			local subtractAmt = math.floor((event.x - finalPos) * 0.5)
+			-- Always move by at least one pixel.
+			if subtractAmt < 1 then
+				subtractAmt = 1
+			end
+
+			event.x = event.x - subtractAmt
+			if event.x < finalPos then
+				event.x = finalPos
+				event.new = false
+			end
+		-- Handle the fade out effect.
+		elseif event.entryTime + KILL_FEED_MESSAGE_TIMEOUT < CurTime() then
+			if event.attackerName then
+				event.attackerColor.a = event.attackerColor.a - FADE_AMOUNT
+			end
+			event.victimColor.a = event.victimColor.a - FADE_AMOUNT
+			event.messageColor.a = event.messageColor.a - FADE_AMOUNT
+			-- Remove message from the kill feed once it is completely transparent.
+			if event.messageColor.a < 0 then
+				table.remove(killFeedEvents, i)
+			end
 		end
 	end
 end)
 
 net.Receive("ph_kill_feed_add", function(len)
-	local killData = net.ReadTable()
-	killData.entryTime = CurTime()
+	local event = net.ReadTable()
+	event.entryTime = CurTime()
+	event.new = true -- New events are events that are in the process of "sliding in" to view.
 
-	table.insert(killFeedEvents, killData)
+	-- Starting x position. Individual draw statements will be relative to this value.
+	event.x = ScrW() -- This will be offscreen.
+
+	-- Calculate the final width of the entire kill feed event.
+	surface.SetFont(FONT)
+	event.eventWidth = surface.GetTextSize(event.victimName) + surface.GetTextSize(" ") + surface.GetTextSize(event.message)
+	if event.attackerName then
+		event.eventWidth = event.eventWidth + surface.GetTextSize(event.attackerName) + surface.GetTextSize(" ")
+	end
+
+	-- New events will start at the top.
+	event.y = 10
+
+	-- Shift existing events downward.
+	for _, kfEvent in ipairs(killFeedEvents) do
+		kfEvent.y = kfEvent.y + draw.GetFontHeight(FONT)
+	end
+
+	table.insert(killFeedEvents, event)
 end)
 
+-- The shadow transparency will be the same as the provided color transparency.
+-- Returns the width and height of the NON SHADOW text.
+local function drawShadowText(text, font, x, y, color)
+	draw.SimpleText(text, font, x + 1, y + 1, Color(0, 0, 0, color.a))
+	return draw.SimpleText(text, font, x, y, color)
+end
+
 local function drawKillFeedHUD()
-	local font = "RobotoHUD-15"
-	for index, event in ipairs(killFeedEvents) do
-		surface.SetFont(font)
-		-- Oldest events should be on the bottom
-		local heightOffset = 10 + ((#killFeedEvents - index) * draw.GetFontHeight(font))
-		local widthOffset = ScrW() - 10 - surface.GetTextSize(event.victimName) - surface.GetTextSize(" ") - surface.GetTextSize(event.message)
-		if event.attackerName then
-			widthOffset = widthOffset - surface.GetTextSize(event.attackerName) - surface.GetTextSize(" ")
+	for _, event in ipairs(killFeedEvents) do
+		local widthOffset = event.x
+
+		-- New events need to have clipping disabled so they can be drawn while partially off-screen.
+		local oldClippingValue
+		if event.new then
+			oldClippingValue = DisableClipping(true)
 		end
 
-		-- TODO: After reorganizing how draw.ShadowText is done, replace all of these double-draws with a single ShadowText
-		-- Attacker or victim name
+		-- If Murder: [Attacker] [Message] [Victim]
+		-- If Suicide: [Victim] [Message]
+
 		if event.attackerName then
-			draw.SimpleText(event.attackerName .. " ", font, widthOffset + 1, heightOffset + 1, Color(0, 0, 0, event.attackerColor.a))
-			widthOffset = widthOffset + draw.SimpleText(event.attackerName .. " ", font, widthOffset, heightOffset, event.attackerColor)
+			widthOffset = widthOffset + drawShadowText(event.attackerName .. " ", FONT, widthOffset, event.y, event.attackerColor)
 		else
-			draw.SimpleText(event.victimName .. " ", font, widthOffset + 1, heightOffset + 1, Color(0, 0, 0, event.victimColor.a))
-			widthOffset = widthOffset + draw.SimpleText(event.victimName .. " ", font, widthOffset, heightOffset, event.victimColor)
+			widthOffset = widthOffset + drawShadowText(event.victimName .. " ", FONT, widthOffset, event.y, event.victimColor)
 		end
 
-		-- Message
-		draw.SimpleText(event.message .. (event.attackerName && " " || ""), font, widthOffset + 1, heightOffset + 1, Color(0, 0, 0, event.messageColor.a))
-		widthOffset = widthOffset + draw.SimpleText(event.message .. (event.attackerName && " " || ""), font, widthOffset, heightOffset, event.messageColor)
+		widthOffset = widthOffset + drawShadowText(event.message .. (event.attackerName && " " || ""), FONT, widthOffset, event.y, event.messageColor)
 
-		-- Attacker name (if present)
 		if event.attackerName then
-			draw.SimpleText(event.victimName, font, widthOffset + 1, heightOffset + 1, Color(0, 0, 0, event.victimColor.a))
-			draw.SimpleText(event.victimName, font, widthOffset, heightOffset, event.victimColor)
+			drawShadowText(event.victimName, FONT, widthOffset, event.y, event.victimColor)
+		end
+
+		if event.new then
+			DisableClipping(oldClippingValue)
 		end
 	end
 end
